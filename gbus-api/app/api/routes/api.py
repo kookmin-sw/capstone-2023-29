063ff569773b5bee3a24094afc9d6256ea67f6fb
carrysocks <choi73737@gmail.com>
2023-04-01 17:41:12 +0900

    modify api router

--- gbus-api/app/api/routes/api.py
+++ gbus-api/app/api/routes/api.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from app.api.routes.user.user import router as user_router
 from app.api.routes.bus.bus import router as bus_router
+from app.api.routes.station.station import router as station_router
 
 api_router = APIRouter()
 api_router.include_router(user_router, tags=["user"], prefix="/v1")
-# api_router.include_router(bus_router, tags=["bus"], prefix="/v1")
+api_router.include_router(bus_router, tags=["bus"], prefix="/v1")
+api_router.include_router(station_router, tags=["station"], prefix="/v1")
