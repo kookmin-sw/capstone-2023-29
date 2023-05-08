b32176fe820c251b420c6d698a12420c85577a93
carrysocks <choi73737@gmail.com>
2023-05-08 23:07:49 +0900

    fix: run.py 및 의존성 수정

--- gbus-api/run.py
+++ gbus-api/run.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="localhost",
+        host="0.0.0.0",
         port=5001,
         reload=True,
         log_level="debug",
