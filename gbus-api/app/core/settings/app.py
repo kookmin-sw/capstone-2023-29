e9b50f5f728514479556433f3a1d98ca78a4a3df
sean <39540655+carrysocks@users.noreply.github.com>
2023-05-12 21:08:06 +0900

    Update app.py

--- gbus-api/app/core/settings/app.py
+++ gbus-api/app/core/settings/app.py
@@ -16,11 +16,7 @@ class AppSettings(BaseSettings):
     VERSION: str = "0.0.1"
     DESCRIPTION: str = "GBUS API Open API Docs"
 
-    WRITE_DB_URL: str = "postgresql://myuser:mypassword@localhost/mydb"
-
-    SECRET_KEY: str = "1893a4f31201b7a012c81623fbf9dd2be82594113266eb377e6c5e8bc84d90de"
-    ALGORITHM: str = "HS256"
-
+    
     @property
     def fastapi_kwargs(self) -> Dict[str, Any]:
         return {
