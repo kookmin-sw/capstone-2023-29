34f62b0e746bb9c056edebfa6f0afc7bf1485022
GoRyne <korsogogii@kookmin.ac.kr>
2023-05-16 14:26:25 +0000

    Fix header

--- gbus-front/src/Header.js
+++ gbus-front/src/Header.js
@@ -1,28 +1,33 @@
 // Header.js
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap/';
+import { useLocation } from 'react-router-dom';
+import './Header.css'
 
 function Header({ token, setToken }) {
+
+    const location = useLocation();
+
   return (
     <Navbar bg="light" variant="light">
-      <Container>
-        <Navbar.Brand href="#home"><img src='/bus_logo.svg' alt='bus_logo'></img></Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/search">Search</Nav.Link>
+      <Container className='head'>
+        <Nav style={{width: '100%', display: 'flex'}}>
+          <Navbar.Brand href="#home"><img src='/bus_logo.svg' alt='bus_logo'></img></Navbar.Brand>
+          <Nav.Link href="/" className={location.pathname === '/' ? 'active' : ''}>Home</Nav.Link>
+          <Nav.Link href="/search" className={location.pathname ==='/search' ? 'active' : ''}>Search</Nav.Link>
           {token ? 
           (
-            <Nav.Link
+              <Nav.Link
               onClick={() => {
-                localStorage.removeItem('token');
-                setToken(null);
-              }}
-            >
+                  localStorage.removeItem('token');
+                  setToken(null);
+                }}
+                >
               Logout
             </Nav.Link>
           ) : (
-            <Nav.Link href="/login">Login</Nav.Link>
-          )}
+              <Nav.Link href="/login">Login</Nav.Link>
+              )}
         </Nav>
       </Container>
     </Navbar>
